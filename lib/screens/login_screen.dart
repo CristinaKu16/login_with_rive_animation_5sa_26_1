1025bf49f531ca055cc767739141ac3b57d13285
CristinaKu16 <cbck16@gmail.com>
2026-02-12 10:46:01 -0600

    feat(rive) wire up isChecking/isHandUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -12,6 +12,16 @@ class _LoginScreenState extends State<LoginScreen> {
   
   //Variable de control para mostrar/ocultar la contraseña
   bool _obscureText = true;
+  
+  //crear el cerebro de la animacion
+  StateMachineController? _controller;
+  //SMI: State Machine Input
+  SMIBool? _isChecking;
+  SMIBool? _isHandsUp;
+  SMITrigger? _trigSuccess;
+  SMITrigger? _trigFail;
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +40,37 @@ class _LoginScreenState extends State<LoginScreen> {
                 height: 200,
                 child: RiveAnimation.asset(
                   'assets/animated_login_bear.riv',
-                ),
+                  stateMachines: ["Login Machine"],
+                  //al iniciar ña animacion
+                  onInit: (artboard) {
+                    _controller=StateMachineController.fromArtboard(artboard, "Login Machine");
+                    
+
+                    //Verifica que inicio bien
+                    if (_controller == null) return;
+                    //Agrega el controlador al tabalero 
+                    artboard.addController(_controller!);
+                    //Vincular variables
+                    _isChecking = _controller!.findSMI("isChecking");
+                    _isHandsUp = _controller!.findSMI("isHandsUp");
+                    _trigSuccess = _controller!.findSMI("trigSuccess");
+                    _trigFail = _controller!.findSMI("trigFail");
+
+                  },
+                  
+                  ),
               ),
           
               //Para separacion
               const SizedBox(height: 10),
-              TextField(
+              TextField( 
+                onChanged: (value) {
+                  if(_isHandsUp !=null){
+                    _isHandsUp !.change(false);
+                  }
+                  if(_isChecking==null) return;
+                    _isChecking!.change(true);
+                },
           
                 //Para mostrar un tipo de teclado
                 keyboardType: TextInputType.emailAddress,
@@ -50,8 +85,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 )
               ), 
               const SizedBox(height: 10),
+              //campo contraseña
+
               TextField(
-                
+                onChanged: (value) {
+                  if(_isChecking !=null){
+                    //no quiero modo chsimoso
+                    _isChecking !.change(false);
+                  }
+                  if(_isHandsUp==null) return;
+                    _isHandsUp!.change(true);
+                },
+
                 obscureText: _obscureText, 
                 decoration: InputDecoration(
                   hintText: 'Password', 
